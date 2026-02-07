97427912b575c915b6b0ca044fbbbb18f5abd404
Prathamesh910 <prathameshgiradkar910@gmail.com>
2026-02-07 22:36:17 +0530

    Added books data...

---
+++ src/data/booksData.js
@@ -0,0 +1,40 @@
+const booksData  = [
+
+
+
+{
+    id: 1,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    category: "Fiction",
+    description: "A story about dreams and destiny.",
+    rating: 4.5,
+  },
+
+
+{
+    id: 2,
+    title: "Atomic Habits",
+    author: "James Clear",
+    category: "Non-Fiction",
+    description: "Build good habits and break bad ones.",
+    rating: 4.8,
+  },
+
+
+{
+    id: 3,
+    title: "Dune",
+    author: "Frank Herbert",
+    category: "Sci-Fi",
+    description: "Epic science fiction novel.",
+    rating: 4.6,
+  },
+
+
+];
+
+
+
+
+export default booksData;
