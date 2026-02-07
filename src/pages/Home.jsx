be94d765370094489f00618b0b6aeecbae53a576
Prathamesh910 <prathameshgiradkar910@gmail.com>
2026-02-07 23:30:49 +0530

    home page with categories and popular books done...

---
+++ src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import { useSelector } from "react-redux";
+
+import { Link } from "react-router-dom";
+
+
+
+function Home(){
+
+
+    const books =  useSelector((state) =>  state.books);
+
+
+
+    return (
+
+<div className="container">
+
+
+<h1> Online Library 📖</h1>
+
+ <h3>Categories</h3>
+      <ul>
+        <li><Link to="/books/Fiction">Fiction</Link></li>
+        <li><Link to="/books/Non-Fiction">Non-Fiction</Link></li>
+        <li><Link to="/books/Sci-Fi">Sci-Fi</Link></li>
+      </ul>
+
+<h3>Popular Books</h3>
+
+<div className="grid">
+{books.slice(0, 2).map((book) => (
+          <div key={book.id} className="card">
+            <h4>{book.title}</h4>
+            <p>{book.author}</p>
+            <Link to={`/book/${book.id}`}>View Details</Link>
+
+
+
+</div>
+        ))}
+      </div>
+
+
+</div>
+
+
+    );
+}
+
+
+
+
+export default Home;
